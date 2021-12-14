ace626ea7fed3313641f5d1a81d48735f1121726
Valts-M <valts114@gmail.com>
2021-12-14 10:40:22 +0200

    fixes so that everything works with the new openvslam version

--- example/run_realsense_t265.cpp
+++ example/run_realsense_t265.cpp
@@ -77,7 +77,7 @@ void stereo_tracking(const std::shared_ptr<openvslam::config>& cfg,
     // create a viewer object
     // and pass the frame_publisher and the map_publisher
 #ifdef USE_PANGOLIN_VIEWER
-    pangolin_viewer::viewer viewer(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
+    pangolin_viewer::viewer viewer(openvslam::util::yaml_optional_ref(cfg->yaml_node_, "PangolinViewer"), &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
 #elif USE_SOCKET_PUBLISHER
     socket_publisher::publisher publisher(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
 #endif
